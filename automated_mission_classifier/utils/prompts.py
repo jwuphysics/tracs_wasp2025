ccc74e7ba73c474d6fcf7bbdf580fb14e93d44ee
John F. Wu <jwuphysics@gmail.com>
2025-10-02 12:43:51 -0400

    clean up prompts

--- automated_mission_classifier/utils/prompts.py
+++ automated_mission_classifier/utils/prompts.py
@@ -10,17 +10,10 @@ logger = logging.getLogger(__name__)
 PROMPT_FILES = {
     'science_system': 'science_system.txt',
     'science_user': 'science_user.txt', 
-    'rerank_science_query': 'rerank_science_query.txt',
-    'telescope_identification_system': 'telescope_identification_system.txt',
-    'telescope_identification_user': 'telescope_identification_user.txt',
-    'telescope_detection_system': 'telescope_detection_system.txt',
-    'telescope_detection_user': 'telescope_detection_user.txt',
     'telescope_classification_system': 'telescope_classification_system.txt',
     'telescope_classification_user': 'telescope_classification_user.txt',
     'rerank_classification_query': 'rerank_classification_query.txt',
-    'primary_telescope_identification_system': 'primary_telescope_identification_system.txt',
-    'primary_telescope_identification_user': 'primary_telescope_identification_user.txt',
-    'primary_telescope_rerank_query': 'primary_telescope_rerank_query.txt',
+    'rerank_science_query': 'rerank_science_query.txt',
 }
 
 
